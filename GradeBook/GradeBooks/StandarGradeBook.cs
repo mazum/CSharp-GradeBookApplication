97a2c7cb1e253d7af8039c38d83629939dba5656
abhishek.mazumder <abhishek.mazumder@crownbet.com.au>
2019-04-26 15:17:19 +1000

    First Commit

---
+++ GradeBook/GradeBooks/StandarGradeBook.cs
@@ -0,0 +1,12 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandarGradeBook : BaseGradeBook
+    {
+        public StandarGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
